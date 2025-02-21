2cbbe1088180ff642a2a730f2bf6e54132c08af8
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-02-22 01:03:38 +0900

    feat: Add batch registration and upgrade tasks for Domains, update README with new commands

--- packages/hardhat/tasks/upgrade/upgradeDomains.ts
+++ packages/hardhat/tasks/upgrade/upgradeDomains.ts
@@ -3,32 +3,34 @@ import {task} from "hardhat/config";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {loadDeployedContractAddresses} from "../../helper/contractsJsonHelper";
 
-task("price", "check price new domain")
-  .addParam("name", "check price name")
-  .addParam("year", "hold years")
+task("upgradeDomains", "upgrade Domains Contract")
+  .addParam("newcontract", "Name of New Contract")
   .setAction(async (taskArgs: any, hre: HardhatRuntimeEnvironment) => {
     console.log(
       "===================================== [START] ===================================== "
     );
+
     // get Contract Address
     const {
-      contracts: {Domains},
+      contracts: {Domains, SampleForwarder},
     } = loadDeployedContractAddresses(hre.network.name);
-    // create Domains contract
-    const domains = await hre.ethers.getContractAt("Domains", Domains);
-
-    // 変数
-    const name = taskArgs.name;
-    const year = taskArgs.year;
 
     try {
-      // check price
-      const result = await domains.price(name, year);
-      console.log(
-        `${taskArgs.name}'s price: ${hre.ethers.formatEther(
-          result.toString()
-        )} ETH`
+      // get new contract factory
+      const NewContract = await hre.ethers.getContractFactory(
+        taskArgs.newcontract
       );
+      // update contract via proxy
+      const newContract = await hre.upgrades.upgradeProxy(
+        Domains,
+        NewContract as any,
+        {
+          constructorArgs: [SampleForwarder],
+        }
+      );
+
+      console.log("Upgraded Contract address:", await newContract.getAddress());
+
       console.log(
         "===================================== [END] ===================================== "
       );
