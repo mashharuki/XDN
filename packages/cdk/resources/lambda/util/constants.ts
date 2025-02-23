3a79f751df62c2b6c1cbf8b5adc5a5857ab4541b
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-02-23 12:05:17 +0900

    add comment

--- packages/cdk/resources/lambda/util/constants.ts
+++ packages/cdk/resources/lambda/util/constants.ts
@@ -1,4 +1,7 @@
+// リレイヤー(EOA)のアドレス
 export const RELAYER_ADDRESS = "0xdec702931fBcaD4f4bB7C1C793E8e5Cdcf7c43A1";
+// Forwarder コントラクトのアドレス
 export const SAMPLEFORWARDER_ADDRESS =
   "0x62CD2CBC855746c16FD16b4E5b34110e1549fc2e";
+// Xenea Test net RPC Endpoint
 export const RPC_URL = "https://rpc-kura.cross.technology";
